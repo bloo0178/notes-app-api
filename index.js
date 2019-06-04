55282f79891b530640a51b524c7a6cb80a6f595a
Jeff Bloom <bloo0178@gmail.com>
2019-06-04 16:20:28 -0400

    Scrapping Express build. Using AWS Lambda functions.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 module.exports.hello = async (event) => {
   return {
@@ -11,4 +11,33 @@ module.exports.hello = async (event) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+};*/
+
+const serverless = require("serverless-http");
+const bodyParser = require("body-parser");
+const projects = require('./handlers/projects');
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+app.use(bodyParser.json({ strict: false }));
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
+app.use('/projects', projects); 
+
+/*
+app.listen(3000, () => {
+	console.log("App listening on port 3000");
+});
+*/
+
+module.exports.handler = serverless(app);
